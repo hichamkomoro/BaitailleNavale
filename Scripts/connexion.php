3cf4f02023f2ace3b09068ca6fa186cbd6126ef6
HichamOujari <67854522+HichamOujari@users.noreply.github.com>
2021-02-17 17:22:44 +0100

    Update connexion.php

--- Scripts/connexion.php
+++ Scripts/connexion.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $db="jeu";
+    $servername = "us-cdbr-east-03.cleardb.com";
+    $username = "b83295eeefefa2";
+    $password = "192e9b16";
+    $db="heroku_da5d9e0b2d90e7c";
     // Create connection
     $conn = new mysqli($servername, $username, $password,$db);
 ?>
